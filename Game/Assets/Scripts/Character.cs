5d30c4ccf927e2c824496ffdaafcab0c8c82be7b
Gupelly <timofejirgashev@gmail.com>
2022-04-14 14:13:50 +0500

    Character Improve
    Починил прыжок и сделал двойной прыжок, персоны\аж больше не упирается в стену

--- Game/Assets/Scripts/Character.cs
+++ Game/Assets/Scripts/Character.cs
@@ -8,11 +8,24 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float speed = 3.0f;
     [SerializeField]
-    private float jumpForce = 15.0f;
+    private float jumpForce = 13.0f;
+    [SerializeField]
+    private float doubleJumpForce = 10.0f;
+
+    private bool unlockDoubleJump = false;
+
+    public LayerMask ground;
+
+    public Transform rightWallCheck;
+    private bool IsRightWall = false;
+
+    public Transform leftWallCheck;
+    private bool IsLeftWall = false;
 
     private bool isGrounded = false;
+    private bool isDoubleJump = false;
 
-    new private Rigidbody2D rb;
+    private Rigidbody2D rb;
     private SpriteRenderer sprite;
 
     private void Awake()
@@ -21,7 +34,7 @@ public class Character : MonoBehaviour
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
-    private void FixideUpdate()
+    private void FixedUpdate()
     {
         CheckGround();
     }
@@ -29,27 +42,37 @@ public class Character : MonoBehaviour
     void Update()
     {
         if (Input.GetButton("Horizontal")) Run();
-        // Jump doesn't work for now(
-        // If delete isGrounded from if, jump will work, but player could jump infinitely, without touching ground, just like flappy bird
-        // Sorry for bad english)
-        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+        if (Input.GetButtonDown("Jump"))
+        {        
+            if (isGrounded) Jump(jumpForce);
+            else if (isDoubleJump)
+            {
+                Jump(doubleJumpForce);
+                isDoubleJump = false;
+            }
+        }
     }
 
     private void Run()
     {
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        if (direction.x > 0 && IsRightWall || direction.x < 0 && IsLeftWall) direction.x = 0;
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
         sprite.flipX = direction.x < 0.0f;
     }
 
-    private void Jump()
+    private void Jump(float jumpForce)
     {
-        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        //rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
     private void CheckGround()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
-        isGrounded = colliders.Length > 1;
+        //Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1F);
+        IsRightWall = Physics2D.OverlapCircle(rightWallCheck.position, 0.01F, ground);
+        IsLeftWall = Physics2D.OverlapCircle(leftWallCheck.position, 0.01F, ground);
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.3F, ground);
+        if (isGrounded) isDoubleJump = true;
     }
 }
