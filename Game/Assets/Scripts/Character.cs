fe36dd86b651e80583098e66a16c65f1a4db2e10
Gupelly <timofejirgashev@gmail.com>
2022-04-15 14:07:13 +0500

    Add dash

--- Game/Assets/Scripts/Character.cs
+++ Game/Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using System;
 
 public class Character : MonoBehaviour
 {
@@ -11,7 +12,14 @@ public class Character : MonoBehaviour
     private float jumpForce = 13.0f;
     [SerializeField]
     private float doubleJumpForce = 10.0f;
+    [SerializeField]
+    private float dashSpeed = 50f;
+    [SerializeField]
+    private float dashDuration = 0.1f;
+    [SerializeField]
+    private float dashCooldown = 1.5f;
 
+    private bool unlockDash = false;
     private bool unlockDoubleJump = false;
 
     public LayerMask ground;
@@ -22,11 +30,19 @@ public class Character : MonoBehaviour
     public Transform leftWallCheck;
     private bool IsLeftWall = false;
 
+    public Transform GroundCheck;
     private bool isGrounded = false;
     private bool isDoubleJump = false;
 
+    private bool isDashing = false;
+    private float currentdashDuration;
+    private bool lockDash = false;
+    private bool jumpAfterDash = false;
+
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+
+
 
     private void Awake()
     {
@@ -41,16 +57,45 @@ public class Character : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButton("Horizontal")) Run();
-        if (Input.GetButtonDown("Jump"))
-        {        
-            if (isGrounded) Jump(jumpForce);
-            else if (isDoubleJump)
+        if (isDashing)
+        {
+            Dash();
+            if (Input.GetButton("Jump")) jumpAfterDash = true;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1") && !lockDash)
             {
-                Jump(doubleJumpForce);
-                isDoubleJump = false;
+                isDashing = true;
+                lockDash = true;
+                Invoke("DashLock", dashCooldown);
+                currentdashDuration = dashDuration;
+                rb.velocity = Vector2.zero;
+                rb.gravityScale = 0;
+            }
+
+            if (Input.GetButton("Horizontal")) Run();
+
+            if (Input.GetButtonDown("Jump") || jumpAfterDash)
+            {
+                jumpAfterDash = false;
+                if (isGrounded) Jump(jumpForce);
+                else if (isDoubleJump)
+                {
+                    Jump(doubleJumpForce);
+                    isDoubleJump = false;
+                }
             }
         }
+    }
+
+    private void CheckGround()
+    {
+        //Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1F);
+        IsRightWall = Physics2D.OverlapCircle(rightWallCheck.position, 0.05F, ground);
+        IsLeftWall = Physics2D.OverlapCircle(leftWallCheck.position, 0.05F, ground);
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.3F, ground);
+        if (isGrounded) isDoubleJump = true;
     }
 
     private void Run()
@@ -58,21 +103,36 @@ public class Character : MonoBehaviour
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
         if (direction.x > 0 && IsRightWall || direction.x < 0 && IsLeftWall) direction.x = 0;
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        //transform.localScale *= new Vector2(Math.Sign(direction.x), 1);
         sprite.flipX = direction.x < 0.0f;
     }
 
     private void Jump(float jumpForce)
     {
+        //rb.velocity = Vector2.zero;
         //rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
-    private void CheckGround()
+    private void Dash()
     {
-        //Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1F);
-        IsRightWall = Physics2D.OverlapCircle(rightWallCheck.position, 0.01F, ground);
-        IsLeftWall = Physics2D.OverlapCircle(leftWallCheck.position, 0.01F, ground);
-        isGrounded = Physics2D.OverlapCircle(transform.position, 0.3F, ground);
-        if (isGrounded) isDoubleJump = true;
+        if (currentdashDuration <= 0)
+        {
+            isDashing = false;
+            rb.gravityScale = 2.8f;
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            currentdashDuration -= Time.deltaTime;
+            if (!sprite.flipX) rb.velocity = Vector2.right * dashSpeed;
+            else rb.velocity = Vector2.left * dashSpeed;
+        }
     }
+
+    private void DashLock()
+    {
+        lockDash = false;
+    }
+
 }
